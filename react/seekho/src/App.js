39f1c90d99d3393e07058d47f41b04d9cc03d39e
Jash Doshi <72159678+c0mpli@users.noreply.github.com>
2022-10-10 08:02:21 +0530

    routes changed

--- react/seekho/src/App.js
+++ react/seekho/src/App.js
@@ -14,8 +14,8 @@ function App() {
     <Routes>
 
     
-    <Route path="/" element={<Home/>}></Route>
-    <Route path="/course" element={<Course/>}></Route>
+    <Route path="seekho/" element={<Home/>}></Route>
+    <Route path="seekho/course" element={<Course/>}></Route>
     </Routes>
     </BrowserRouter>
     </>
